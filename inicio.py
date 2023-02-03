db31f856a93ee802b3b6ac8ec02a1a743affaeda
Alexis Puglia <alexis.puglia@gmail.com>
2023-02-03 20:31:11 -0300

    Incluída biblioteca geocoder, automatizando a localização (cidade) do usuário

--- inicio.py
+++ inicio.py
@@ -1,3 +1,5 @@
+import geocoder
+
 nome1 = input("Insira o seu nome completo: ")
 nome2 = input("Insira o nome da pessoa que realizou o pagamento: ")
 valor = input("Insira o valor que foi recebido: ")
@@ -5,6 +7,7 @@ servico = input("Informe por gentileza qual foi o serviço realizado: ")
 local = input("Informe em qual cidade e estado foi realizado o serviço: ")
 dia_data = input("Informe a data em que ocorreu o serviço (dia, mês e ano): ")
 
+local_recibo = geocoder.ip('me')
 
 print(f"{'RECIBO' : ^150}")
 
@@ -24,6 +27,7 @@ print("")
 print("")
 print("")
 
+print(local_recibo.city, ",")
 
 print("")
 print("")
