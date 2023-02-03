e2d27685ab79b7e1708c8bbdaccc10c5ca30b2d8
Alexis Puglia <alexis.puglia@gmail.com>
2023-02-03 20:23:00 -0300

    Estrutura básica do projeto

---
+++ inicio.py
@@ -0,0 +1,39 @@
+nome1 = input("Insira o seu nome completo: ")
+nome2 = input("Insira o nome da pessoa que realizou o pagamento: ")
+valor = input("Insira o valor que foi recebido: ")
+servico = input("Informe por gentileza qual foi o serviço realizado: ")
+local = input("Informe em qual cidade e estado foi realizado o serviço: ")
+dia_data = input("Informe a data em que ocorreu o serviço (dia, mês e ano): ")
+
+
+print(f"{'RECIBO' : ^150}")
+
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+
+print(f"Informo para todos os fins que recebi de {nome2} a quantia de R$ {valor}, correspondente a {servico}, serviço este realizado em {local}, em {dia_data}")
+
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+
+
+print("")
+print("")
+print("")
+
+
+print(f"{'___________________________________' : ^150}")
+
+
+
+
+
+
